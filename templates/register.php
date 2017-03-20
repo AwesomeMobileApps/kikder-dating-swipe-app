5759f4102480b03f2898992747b3930c6e34288b
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-03-20 22:58:36 +0000

    Remove label tag/male form simpler + add placeholder attr instead

--- templates/register.php
+++ templates/register.php
@@ -8,24 +8,20 @@
                     <?php echo $error; ?>
                     <form class="form" method="POST" action="<?php echo site_url('create');?>">
                         <div class="form-group">
-                            <label for="kik">Kik Username:</label>
-                            <input type="text" name="kik_username" class="form-control login-control">
+                            <input type="text" name="kik_username" placeholder="Kik Username" class="form-control login-control">
                         </div>
 
                         <div class="form-group">
-                            <label for="email">Your gender:</label>
-<select class="form-control input-lg" name="user_gender">
-<option value="1" title="Male">Male</option>
-<option value="2" title="Female">Female</option>
-</select>
+                            <select class="form-control input-lg" name="user_gender">
+                                <option value="1" title="Male">Male</option>
+                                <option value="2" title="Female">Female</option>
+                            </select>
                         </div>
                         <div class="form-group">
-                            <label for="email">Your email:</label>
-                            <input type="email" name="user_email" class="form-control login-control">
+                            <input type="email" name="user_email" placeholder="Your email" class="form-control login-control">
                         </div>
                         <div class="form-group">
-                            <label for="password">Your password:</label>
-                            <input type="password" name="user_pass" class="form-control login-control">
+                            <input type="password" name="user_pass" placeholder="Your password" class="form-control login-control">
                         </div>
                         <div class="text-center"><div class="g-recaptcha" data-sitekey="6LeRGxcTAAAAAO_qGJGcQs6aPu8kbcELfIIlqBnA"></div></div>
                         <br />
