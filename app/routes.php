78d2ee17632c1879bf8de5ddfc1bb261b5e01e29
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-03-23 23:41:15 +0000

    Currently we dont need to add users, so disable that route for security reason

--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,5 @@ $route->add('/','Kik@index');
 $route->add('/signin', 'Kik@signIn');
 $route->add('/create', 'Kik@createAcc');
 $route->add('/signout', 'Kik@signOut');
-$route->add('/addUsers', 'Kik@userPictures');
 $route->add('/loadUsers', 'Kik@loadUsers');
 $route->add('/forgot', 'Kik@forgot');
