422c2920868c1d4def13715342c915d0257de072
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-26 13:50:27 +0100

    Replace include_once() to just require since they are mandatory and "once" isnt needed for app.php

--- app/app.php
+++ app/app.php
@@ -8,14 +8,14 @@ function site_url($var = '') {
     return SITE_URL;
 }
 
-include_once('core/Route.php');
-include_once('core/Database.php');
-include_once('core/Main.php');
-include_once('core/View.php');
-include_once('core/User.php');
-include_once('core/Session.php');
-include_once('core/Input.php');
-include_once('core/Hash.php');
+require 'core/Route.php';
+require 'core/Database.php';
+require 'core/Main.php';
+require 'core/View.php';
+require 'core/User.php';
+require 'core/Session.php';
+require 'core/Input.php';
+require 'core/Hash.php';
 
 function autoloadController($className) {
     $filename = './app/controllers/' . $className . '.php';
