29be72a7d9017c13856cc0860b27101e296eadc9
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-28 01:13:33 +0100

    Move config constants from index its own file
    (in a new config folder)

---
+++ app/config/site.php
@@ -0,0 +1,8 @@
+<?php
+
+// Edit the following details by yours
+define('SITE_URL', 'http://kikornot.com/');
+define('SITE_NAME', 'Kik or Not!');
+define('ADMIN_EMAIL', 'admin@kikornot.com');
+define('RECAPTCHA_SECRET_KEY', '6LeRGxcTAAAAANuetpENkUiqHG2e7mjgschpySkN'); // https://www.google.com/recaptcha/admin
+define('GOOGLE_ANALYTICS_ID', 'UA-<YOUR-GOOGLE-ANALYTICS-ID>'); // https://analytics.google.com
--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-// Edit the following details by yours
-define('SITE_URL', 'http://kikornot.com/');
-define('SITE_NAME', 'Kik or Not!');
-define('ADMIN_EMAIL', 'admin@kikornot.com');
-define('RECAPTCHA_SECRET_KEY', '6LeRGxcTAAAAANuetpENkUiqHG2e7mjgschpySkN');
-define('GOOGLE_ANALYTICS_ID', 'UA-<YOUR-GOOGLE-ANALYTICS-ID>');
 
 require 'app/app.php';
