a6b4917f9b4d99b7fdd2d27d9def4381ac6a9a8e
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-29 02:46:21 +0100

    Fix bug that was always showing 404 page

--- app/core/Route.php
+++ app/core/Route.php
@@ -50,12 +50,17 @@ class Route
                         exit;
                     }
                 }
-            } else {
-                // TODO From Kik ctrl, call a nice 404 page method
-                header('HTTP/1.1 404 Not Found');
-                echo 'Page Not Found!';
-                exit;
             }
         }
+        // If didn't return a found method, we display the 404 page
+        $this->display404();
+    }
+
+    private function display404()
+    {
+        // TODO From Kik ctrl, call a nice 404 page method
+        header('HTTP/1.1 404 Not Found');
+        echo 'Page Not Found!';
+        exit;
     }
 }
