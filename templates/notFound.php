7863ff035808dd0f00fe827207038e9a86e81252
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-30 11:24:30 +0100

    Add NotFound view file

---
+++ templates/notFound.php
@@ -0,0 +1,17 @@
+<div class="createAcc">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel">
+                    <h2>Page Not Found!</h2>
+                    <p><strong>Sorry, we weren't able to find the page you requested.</strong></p>
+                    <p><strong><em>Suggestions:</em></strong></p>
+                    <ul>
+                        <li><a href="<?php echo site_url(); ?>">Main Page</a></li>
+                        <li><a href="javascript:void(0);" onclick="history.go(-1);">Go back to the previous page</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
