be8d40b050da1249b3554e28ae2b8efe684c0260
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-20 23:07:19 +0100

    Dont show the recaptcha example/test key

--- app/config/site.php
+++ app/config/site.php
@@ -4,5 +4,5 @@
 define('SITE_URL', 'http://kikornot.com/');
 define('SITE_NAME', 'Kik or Not!');
 define('ADMIN_EMAIL', 'admin@kikornot.com');
-define('RECAPTCHA_SECRET_KEY', '6LeRGxcTAAAAANuetpENkUiqHG2e7mjgschpySkN'); // https://www.google.com/recaptcha/admin
+define('RECAPTCHA_SECRET_KEY', 'YOUR-RECAPTCHA-KEY'); // https://www.google.com/recaptcha/admin
 define('GOOGLE_ANALYTICS_ID', 'UA-<YOUR-GOOGLE-ANALYTICS-ID>'); // https://analytics.google.com
