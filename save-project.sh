c4ff9105d4e79d869effc971afff64a922f02bd5
Pierre-Henry Soria <hi@ph7.me>
2018-03-04 10:24:08 +0000

    Backup easily Kikder project on different Git repos

---
+++ save-project.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Save the git project to a specific repo (e.g. github, gitlab, ...)
+function save-project() {
+    git remote rm origin
+    git remote add origin $1
+    git push
+}
+
+save-project git@bitbucket.org:pH_7/kikornot.git
+save-project git@gitlab.com:pH-7/kikder-dating-swipe-app.git
+save-project git@github.com:AwesomeMobileApps/kikder-dating-swipe-app.git
