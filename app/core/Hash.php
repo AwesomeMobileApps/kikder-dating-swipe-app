f82b798d3fa6ac9085064a62bbd11fa897754042
Pierre-Henry Soria <hi@ph7.me>
2018-07-01 02:35:38 +0200

    [Security Fix] Prevent timing attacks by comparing the two hashes using the same time whether they're equal or not

--- app/core/Hash.php
+++ app/core/Hash.php
@@ -22,13 +22,43 @@ class Hash
     /**
      * Compares a string to a hash.
      *
-     * @param  string $string The string
-     * @param  string $hash The Hash
+     * @param string $string The string
+     * @param string $hash The Hash
      *
-     * @return boolean        Does it match?
+     * @return bool Does it match?
      */
     public static function compare($string, $hash)
     {
-        return crypt($string, $hash) === $hash;
+        $existingHash = crypt($string, $hash);
+
+        return self::hashEquals($existingHash, $hash);
+    }
+
+    /**
+     * @param string $knownString
+     * @param string $userString
+     *
+     * @return bool
+     */
+    private static function hashEquals($knownString, $userString)
+    {
+        if(!function_exists('hash_equals')) {
+            // For PHP < 5.6
+            function hash_equals($str1, $str2)
+            {
+                if(strlen($str1) != strlen($str2)) {
+                    return false;
+                }
+
+                $result = $str1 ^ $str2;
+                $return = 0;
+                $total = strlen($result) - 1;
+                for($i = $total; $i >= 0; $i--) {
+                    $return |= ord($result[$i]);
+                }
+
+                return !$return;
+            }
+        }
     }
 }
