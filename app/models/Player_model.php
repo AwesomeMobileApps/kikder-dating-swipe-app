66ffcdee77516e48f5fdd190bac65401f2e3caf8
Pierre-Henry Soria <hi@ph7.me>
2018-12-15 13:59:09 +1300

    Fix docBlock type names

--- app/models/Player_model.php
+++ app/models/Player_model.php
@@ -6,9 +6,9 @@ class Player_Model
     protected $table = 'users';
 
     /**
-     * @param $select
-     * @param $column
-     * @param $value
+     * @param string $select
+     * @param string $column
+     * @param string $value
      *
      * @return stdClass
      */
@@ -20,17 +20,17 @@ class Player_Model
     }
 
     /**
-     * @param $dribbbleId
-     * @param $fullName
-     * @param $avatar
-     * @param $permission
-     * @param $username
-     * @param $code
+     * @param string $dribbbleId
+     * @param string $fullName
+     * @param string $avatar
+     * @param string $permission
+     * @param string $username
+     * @param string $code
      */
     public function createPlayer($dribbbleId, $fullName, $avatar, $permission, $username, $code)
     {
         $sqlQuery = "INSERT INTO users (user_token, user_name, can_upload_shot, user_dribbble, user_avatar, user_dribbble_id)
-          VALUES(:core, :fullName, :permission, :username, :avatar, :dribbbleId)";
+          VALUES (:core, :fullName, :permission, :username, :avatar, :dribbbleId)";
 
         $binds = [
             ':core' => $code,
